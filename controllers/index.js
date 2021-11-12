ef78ef9f6a9bcefecd41f5375d564b56c26cb658
Nejc7777 <47789821+Nejc7777@users.noreply.github.com>
2021-11-12 20:48:23 +0100

    asd

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+console.log("Environment variable: " + process.env.NODE_ENV);
+
 const REDIS_PORT = 6379;
 
 var Promise = require("bluebird");
